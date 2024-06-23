7f0091ab818e885f2865fc7e31f3063accefbe47
Shivam Garg <sgshivamgarg8@gmail.com>
2024-06-23 18:28:39 +0530

    docs: add docs

--- src/app/utils/date.class.ts
+++ src/app/utils/date.class.ts
@@ -1,6 +1,15 @@
 import { DateObj } from '../models/date.model';
 
 export class AppDate {
+  /**
+   * Get Date form Date object
+   *
+   * e.g. { day: 4, month: 5, year:2024 } => Date
+   *
+   * @static
+   * @param {DateObj} dateObj
+   * @returns {Date}
+   */
   static getDateFromDateObj(dateObj: DateObj): Date {
     if (dateObj === null || dateObj === undefined) {
       throw new Error('dateObj is undefined or null');
@@ -9,6 +18,15 @@ export class AppDate {
     return new Date(dateObj.year, dateObj.month - 1, dateObj.day);
   }
 
+  /**
+   * Get Date object from Date
+   *
+   * e.g. Date => { day: 4, month: 5, year:2024 }
+   *
+   * @static
+   * @param {Date} date
+   * @returns {DateObj}
+   */
   static getDateObjFromDate(date: Date): DateObj {
     if (date === null || date === undefined) {
       throw new Error('date is undefined or null');
